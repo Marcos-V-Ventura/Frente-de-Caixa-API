b108a31c68bd04be08a68196b89a27ed9daa840d
Vitor Hugo <vitorhugovieirabraga@gmail.com>
2023-10-31 18:18:34 -0300

    fixed: route listcategories

--- src/router.js
+++ src/router.js
@@ -1,17 +1,27 @@
 const { Router } = require("express");
-const { registerUser, login, editUser, userProfile } = require("./controllers/users");
-const { registerFields, auth, loginFields, editFields } = require("./middlewares/md_users");
+const {
+  registerUser,
+  login,
+  editUser,
+  userProfile,
+} = require("./controllers/users");
+const {
+  registerFields,
+  auth,
+  loginFields,
+  editFields,
+} = require("./middlewares/md_users");
 const { listCategories } = require("./controllers/categories");
 
 const router = Router();
 
 router.post("/usuario", registerFields, registerUser);
 router.post("/login", loginFields, login);
+router.get("/categoria", listCategories);
 
 router.use(auth);
 
 router.put("/usuario", editFields, editUser);
 router.get("/usuario", userProfile);
-router.get("/categoria", listCategories);
 
 module.exports = router;
