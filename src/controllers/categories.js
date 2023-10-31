80cc0cf65ca9d4a987238aa1557e6791bf568b2a
Erico dos Santos Bispo <erico.desenvolvedor@gmail.com>
2023-10-31 11:27:40 -0300

    Correcao Categorias

--- src/controllers/categories.js
+++ src/controllers/categories.js
@@ -1,9 +1,9 @@
-const knex = require("./conection");
+const knex = require("../connection");
 
 const listCategories = async (req, res) => {
   try {
     const categories = await knex("categorias");
-    return res.status(200).json(categories.rows);
+    return res.status(200).json(categories);
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
