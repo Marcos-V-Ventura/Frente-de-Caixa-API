f6a5aa2f092574fcbfcebe8f96c66d4797e86bbd
Vitor Hugo <vitorhugovieirabraga@gmail.com>
2023-11-02 21:32:55 -0300

    update: dump

--- dump.sql
+++ dump.sql
@@ -11,6 +11,27 @@ create table categorias (
     descricao text not null
 );
 
+create table produtos(
+    id serial primary key,
+    descricao text,
+    quantidade_estoque integer not null,
+    valor integer not null,
+    categoria_id integer not null references categorias(id)
+)
+
+create table clientes(
+    id serial primary key,
+    nome text not null,
+    email text unique not null,
+    cpf text unique not null,
+    cep text,
+    rua text,
+    numero text,
+    bairro text,
+    cidade text,
+    estado text
+)
+
 INSERT INTO categorias (descricao)
 VALUES ('Informática'),
     ('Celulares'),
