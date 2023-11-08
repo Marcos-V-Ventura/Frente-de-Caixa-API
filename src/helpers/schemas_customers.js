e6d830f06df524214750ce56e685a63c3cf3881e
Vitor Hugo <vitorhugovieirabraga@gmail.com>
2023-11-08 20:28:15 -0300

    update

--- src/helpers/schemas_customers.js
+++ src/helpers/schemas_customers.js
@@ -4,9 +4,19 @@ const { typeErrorString, typeErrorNumber } = require("./errorMessages");
 yup.setLocale(pt);
 
 const validateRegisterAndUpdateCustomer = yup.object({
-    nome: yup.string().required(),
-    email: yup.string().email().required(),
-    cpf: yup.string().length(11).required(),
+  nome: yup.string().required().typeError(typeErrorString("Nome")).strict(),
+  email: yup
+    .string()
+    .email()
+    .typeError(typeErrorString("Email"))
+    .required()
+    .strict(),
+  cpf: yup
+    .string()
+    .length(11)
+    .typeError(typeErrorString("CPF"))
+    .required()
+    .strict(),
 });
 
 module.exports = validateRegisterAndUpdateCustomer;
