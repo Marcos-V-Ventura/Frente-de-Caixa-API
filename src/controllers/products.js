e3679f17928729a3938f1b6d55680f794e650071
Erico dos Santos Bispo <erico.desenvolvedor@gmail.com>
2023-11-08 11:29:37 -0300

    Listrar e detalhar produtos

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -5,33 +5,42 @@ const errorMessages = require("../helpers/errorMessages");
 yup.setLocale(pt);
 
 const registerProduct = async (req, res) => {
-    const { descricao, quantidade_estoque, valor, categoria_id } = req.body;
-    try {
-        await knex('produtos').insert({ descricao, quantidade_estoque, valor, categoria_id });
+  const { descricao, quantidade_estoque, valor, categoria_id } = req.body;
+  try {
+    await knex("produtos").insert({
+      descricao,
+      quantidade_estoque,
+      valor,
+      categoria_id,
+    });
 
-        return res.status(201).json();
-    } catch (error) {
-        if(error.name == "ValidationError")
-            return res.status(400).json({ mensagem: error.message });
+    return res.status(201).json();
+  } catch (error) {
+    if (error.name == "ValidationError")
+      return res.status(400).json({ mensagem: error.message });
 
-        return res.status(500).json({ error: error.message, message: errorMessages.server});
-    }
-}
+    return res
+      .status(500)
+      .json({ error: error.message, message: errorMessages.server });
+  }
+};
 
 const editProduct = async (req, res) => {
-    const { descricao, quantidade_estoque, valor, categoria_id } = req.body;
-    const { id } = req.params;
-    try {
-        const [product] = await knex("produtos")
-            .update({ descricao, quantidade_estoque, valor, categoria_id  })
-            .where({ id })
-            .returning("*");
-        
-        return res.status(200).json(product);
-    } catch (error) {
-        return res.status(500).json({ error: error.message, message: errorMessages.server });
-    }
-}
+  const { descricao, quantidade_estoque, valor, categoria_id } = req.body;
+  const { id } = req.params;
+  try {
+    const [product] = await knex("produtos")
+      .update({ descricao, quantidade_estoque, valor, categoria_id })
+      .where({ id })
+      .returning("*");
+
+    return res.status(200).json(product);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: error.message, message: errorMessages.server });
+  }
+};
 
 const deleteProductById = async (req, res) => {
   const { id } = req.params;
@@ -52,5 +61,46 @@ const deleteProductById = async (req, res) => {
       .json({ mensagem: errorMessages.server, error: message });
   }
 };
+const listProducts = async (req, res) => {
+  const { categoria_id } = req.query;
 
-module.exports = { deleteProductById, registerProduct, editProduct };
+  try {
+    if (categoria_id) {
+      const listProductsByCategory = await knex("produtos").where({
+        categoria_id,
+      });
+      return res.status(200).json(listProductsByCategory);
+    }
+    const productList = await knex("produtos");
+    return res.status(200).json(productList);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ mensagem: errorMessages.server, error: message });
+  }
+};
+
+const detailProducts = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const findProduct = await knex("produtos").where({ id }).first();
+    if (!findProduct) {
+      return res.status(400).json({ mensagem: errorMessages.invalidProducts });
+    }
+
+    return res.status(200).json(findProduct);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ mensagem: errorMessages.server, error: message });
+  }
+};
+
+module.exports = {
+  deleteProductById,
+  registerProduct,
+  editProduct,
+  listProducts,
+  detailProducts,
+};
--- src/router.js
+++ src/router.js
@@ -1,11 +1,35 @@
 const { Router } = require("express");
-const { registerUser, login, editUser, userProfile, getAllClients, getClientById } = require("./controllers/users");
+const {
+  registerUser,
+  login,
+  editUser,
+  userProfile,
+  getAllClients,
+  getClientById,
+} = require("./controllers/users");
 const { listCategories } = require("./controllers/categories");
 const { registerCustomer, editCustomer } = require("./controllers/customers");
-const { customerUpdateFields,customerRegisterFields } = require("./middlewares/md_customers");
-const { loginFields, editFields, auth, registerFields } = require("./middlewares/md_users");
-const { deleteProductById, registerProduct, editProduct } = require("./controllers/products");
-const { productFields, validateProdutoId } = require("./middlewares/md_products");
+const {
+  customerUpdateFields,
+  customerRegisterFields,
+} = require("./middlewares/md_customers");
+const {
+  loginFields,
+  editFields,
+  auth,
+  registerFields,
+} = require("./middlewares/md_users");
+const {
+  deleteProductById,
+  registerProduct,
+  editProduct,
+  listProducts,
+  detailProducts,
+} = require("./controllers/products");
+const {
+  productFields,
+  validateProdutoId,
+} = require("./middlewares/md_products");
 
 const router = Router();
 
@@ -19,12 +43,15 @@ router.put("/usuario", editFields, editUser);
 router.get("/usuario", userProfile);
 router.get("/categoria", listCategories);
 router.get("/cliente", getAllClients);
-router.get("/cliente/:id", getClientById); 
+router.get("/cliente/:id", getClientById);
 router.delete("/produto/:id", deleteProductById);
 router.post("/produto", productFields, registerProduct);
 router.put("/produto/:id", productFields, validateProdutoId, editProduct);
 
-router.post('/cliente', customerRegisterFields ,registerCustomer)
-router.put('/cliente/:id', customerUpdateFields, editCustomer)
+router.get("/produto", listProducts);
+router.get("/produto/:id", detailProducts);
+
+router.post("/cliente", customerRegisterFields, registerCustomer);
+router.put("/cliente/:id", customerUpdateFields, editCustomer);
 
 module.exports = router;
