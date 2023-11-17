7d08fe2832f2d5ad9e7e319f9b1f310ab311ba28
Marcos-V-Ventura <marcos.vinicius.13@hotmail.com>
2023-11-16 23:19:20 -0300

    add: yupMessage

--- src/helpers/yupMessages.js
+++ src/helpers/yupMessages.js
@@ -1,42 +1,13 @@
 const yup = require("yup");
 const { pt } = require("yup-locales");
-const {
-  typeErrorString,
-  typeErrorNumber,
-  typeErrorArray,
-} = require("./errorMessages");
 yup.setLocale(pt);
 
-const validateAllFieldsOrders = yup.object({
-  cliente_id: yup
-    .number()
-    .required()
-    .typeError(typeErrorNumber("Cliente_id"))
-    .strict(),
-  observacao: yup.string().typeError(typeErrorString("Observacao")).strict(),
-  pedido_produtos: yup
-    .array()
-    .of(
-      yup.object({
-        produto_id: yup
-          .number()
-          .required()
-          .typeError(typeErrorNumber("Produto_id"))
-          .strict(),
-        quantidade_produto: yup
-          .number()
-          .required()
-          .typeError(typeErrorNumber("Quantidade_produto"))
-          .strict(),
-      })
-    )
-    .min(
-      2,
-      "Pedido_produtos: Os campos produto_id e quantidade_produto são obrigatórios"
-    )
-    .required()
-    .typeError(typeErrorArray("Pedido_produtos"))
-    .strict(),
-});
+const yupMessages = {
+    requiredProductId: "Produto_id é um campo necessário.",
+    requiredQuantityProduct: "Quantidade_produto é um campo necessário.",
+    minOrderProduct: "Pedido_produtos: Os campos produto_id e quantidade_produto são necessários.",
+    minValueProduct: "O valor não pode ser negativo ou zero!",
+    minQuantityStock: "Quantidade_estoque não pode ser negativo ou zero! "
+}
 
-module.exports = validateAllFieldsOrders;
+module.exports = yupMessages;
