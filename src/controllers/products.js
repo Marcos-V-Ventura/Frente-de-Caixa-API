6dd42f66f56c3bc926b1e46713c11b96fd6d05ad
Vitor Hugo <vitorhugovieirabraga@gmail.com>
2023-11-16 23:40:12 -0300

    fix imagem

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -5,7 +5,6 @@ const utils = require("../helpers/utils");
 const registerProduct = async (req, res) => {
   const { descricao, quantidade_estoque, valor, categoria_id } = req.body;
   const { file } = req;
-
   try {
     const { Location: produto_imagem } = await utils.setProductImage(file);
 
@@ -31,6 +30,49 @@ const updateProduct = async (req, res) => {
   const { file } = req;
 
   try {
+    if (!file) {
+      const [product] = await knex("produtos")
+        .update({
+          descricao,
+          quantidade_estoque,
+          valor,
+          categoria_id,
+        })
+        .where({ id })
+        .returning("*");
+
+      return res.status(200).json(product);
+    }
+
+    const foundImage = await knex("produtos")
+      .select("produto_imagem")
+      .where({ id })
+      .returning("*");
+
+    if (foundImage) {
+      await utils.deleteImage(foundImage);
+
+      await knex("produtos")
+        .update({ produto_imagem: null })
+        .where({ id })
+        .returning("*");
+
+      const { Location: produto_imagem } = await utils.setProductImage(file);
+
+      const [product] = await knex("produtos")
+        .update({
+          descricao,
+          quantidade_estoque,
+          valor,
+          categoria_id,
+          produto_imagem,
+        })
+        .where({ id })
+        .returning("*");
+
+      return res.status(200).json(product);
+    }
+
     const { Location: produto_imagem } = await utils.setProductImage(file);
 
     const [product] = await knex("produtos")
