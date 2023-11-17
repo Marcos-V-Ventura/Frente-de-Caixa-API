1dd4d3545629e664e593404f418698b7f838743b
Vitor Hugo <vitorhugovieirabraga@gmail.com>
2023-11-17 11:29:04 -0300

    teste nodemailer

--- src/nodemailer.js
+++ src/nodemailer.js
@@ -1,22 +1,23 @@
 const nodemailer = require("nodemailer");
-require("dotenv").config()
+require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
 const send = (to, subject, body) => {
-    transporter.sendMail({
-        from: process.env.EMAIL_FROM,
-        to,
-        subject,
-        text: body
-    })
-}
+  transporter.sendMail({
+    from: process.env.EMAIL_FROM,
+    to,
+    subject,
+    text: body,
+  });
+};
 
-module.exports = { transporter, send }
+module.exports = { transporter, send };
